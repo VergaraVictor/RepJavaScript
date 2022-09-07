f755137684d9857a4104db8590ee9c022b0afe55
vvergara-hub <victorvergara1531@gmail.com>
2022-09-06 21:42:35 -0500

    UpdateProtipRetorno

--- JavaScript/01-fundamentos/assets/js/protip-retorno.js
+++ JavaScript/01-fundamentos/assets/js/protip-retorno.js
@@ -41,4 +41,31 @@ console.log({ casado, vivo, nombre, saludo });
 const { apellido: nuevoApellido } = crearPersona( 'Bruce', 'Waine');
 console.log({ nuevoApellido });
 
-//
+const tony = {
+    nombre: 'Tony Stark',
+    codeName: 'Ironman',
+    vivo: false,
+    edad: 40,
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+};
+
+// const imprimePropiedades = (personaje) => {
+//    console.log( 'nombre:',personaje.nombre);
+//    console.log( 'codeName:',personaje.codeName);
+//    console.log( 'vivo:',personaje.vivo);
+//    console.log( 'edad:',personaje.edad);
+//    console.log( 'trajes:',personaje.trajes);
+// }
+
+const imprimePropiedades = ({ nombre, codeName, vivo, edad = 15, trajes }) => {
+
+    // edad = edad || 0; Esto funciona si es un solo campo si son muchos no es conveniente
+
+    console.log({ nombre });
+    console.log({ codeName });
+    console.log({ vivo });
+    console.log({ edad });
+    console.log({ trajes });
+}
+
+imprimePropiedades(tony);
