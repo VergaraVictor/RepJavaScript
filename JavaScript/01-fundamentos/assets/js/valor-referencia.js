b94729472e98d9038ffc130a1a6caa4099809d5d
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2022-09-08 12:28:08 -0500

    UpdateValorReferencia

--- JavaScript/01-fundamentos/assets/js/valor-referencia.js
+++ JavaScript/01-fundamentos/assets/js/valor-referencia.js
@@ -10,6 +10,17 @@ console.log({ a, b });
 
 
 let juan = { nombre: 'Juan' };
-let ana = juan;
+let ana = {juan};
+ana.nombre = 'Ana';
 
 console.log({ juan, ana })
+
+const cambiaNombre = ( persona ) => {
+    persona.nombre = 'Tony';
+    return persona;
+}
+
+let peter = { nombre: 'Peter'};
+let tony = cambiaNombre( peter );
+
+console.log({ peter, tony });
