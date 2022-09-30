851c5591b431c2412a2fdeeaa19c770522e20bcb
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2022-09-29 19:22:33 -0500

    UpdateLogicaBooleana
    Se termina logica booleana

--- JavaScript/01-fundamentos/assets/js/logica-booleana.js
+++ JavaScript/01-fundamentos/assets/js/logica-booleana.js
@@ -16,7 +16,7 @@ console.log(!!false); // true se pueden utilizar de acuerdo a loq ue se requiera
 
 console.log(!regresaFalse()); //true
 
-//Operador And
+//Operador And &&
 
 console.log('And') // true = si todos los valores son verdaderos
 console.log(true && true); // true
@@ -24,4 +24,42 @@ console.log(true && false); // false
 console.log(true && !false); // true
 
 console.log('=========');
-console.log(regresaFalse() && regresaTrue() );
+console.log(regresaFalse() && regresaTrue()); //false
+console.log(regresaTrue() && regresaFalse()); //false
+
+console.log('====&&====')
+regresaFalse() && regresaTrue();
+
+console.log( '4 condiciones', true && true && true && false); //false
+
+// Operador OR ||
+
+console.warn('OR'); // solo necesita que una de las condiciones regrese true el resto ya no importa
+console.log(true || false); //true
+console.log(false || false); //false
+
+console.log( regresaTrue() || regresaFalse() );
+console.log('4 condiciones', true || true || true || false); //true
+
+console.warn('Asiganciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = false && 'Hola Mundo' && 150; // false
+const a2 = 'Hola' && 'Mundo' && soyFalso && true; //
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo' || true;
+
+console.log({ a1, a2, a3, a4, a5 });
+
+if (regresaFalse() && regresaTrue() && (true || false || true) ) {
+    console.log('Hacer algo');
+} else {
+    console.log('Hacer otra cosa');
+}
+
+
+
