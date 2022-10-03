191a06df194b24ce2896a1df2816892d3e0056ea
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2022-10-03 18:07:01 -0500

    Updatejuegosblackjack
    deacuerod el update

---
+++ JavaScript/02-blackjack/assets/js/juego.js
@@ -0,0 +1,21 @@
+/**
+ * 2C = Two of Clubs (Tréboles)
+ * 2D = Two of Diamonds (Diamantes)
+ * 2H = Two of Hearts (Corazones)
+ * 2S = Two of Spades (Espadas)
+ */
+
+let deck = [];
+const
+
+const crearDeck = () => {
+
+    for( let i = 2; i <= 10; i++ ){
+        // deck.push( i + 'C' );
+    }
+
+    console.log( deck );
+}
+
+crearDeck();
+
