f9374c2824f8520a90a677e632c8a3e94d63649e
VergaraVictor <68487222+VergaraVictor@users.noreply.github.com>
2022-10-05 22:50:13 -0500

    UpdatePedirCarta

--- JavaScript/02-blackjack/assets/js/juego.js
+++ JavaScript/02-blackjack/assets/js/juego.js
@@ -8,7 +8,7 @@
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
-
+// esta funcion crea un nuevo deck o baraja
 const crearDeck = () => {
 
     for (let i = 2; i <= 10; i++){
@@ -23,8 +23,54 @@ const crearDeck = () => {
         }
     }
 
+    // console.log( deck );
+    deck = _.shuffle( deck );
     console.log( deck );
+    return deck;
 }
 
 crearDeck();
 
+// Esta funcion me permite tomar una carta
+const pedirCarta = () => {
+    
+    if ( deck.length === 0 ){
+        throw 'No hay cartas en el deck';
+        // console.log('????'); en este caswo no es posible ver este consol log por el trhow
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); // carta debe de ser de la baraja
+    return carta;
+}
+
+
+// pedirCarta();
+
+const valorCarta = ( carta ) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    return ( isNaN( valor )) ?
+            ( valor === 'A' ) ? 11 : 10
+            : valor * 1;
+
+            //El codigo de arriba equivale a lo comentareado abajo solo que simplificado
+    // let puntos = 0;
+    // console.log({ valor });
+    // if( isNaN( valor ) ) {
+
+    //     puntos = ( valor === 'A' ) ? 11 : 10;
+
+    // } else {
+    //     puntos = valor * 1;
+    // }
+
+    // console.log(puntos);
+
+
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor });
