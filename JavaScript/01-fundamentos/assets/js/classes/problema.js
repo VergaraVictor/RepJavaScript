b96d698dfcf626865a3c37958c27c25c9ad0b5df
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2022-11-01 21:39:46 -0500

    UpdateInicioDeClasses

---
+++ JavaScript/01-fundamentos/assets/js/classes/problema.js
@@ -0,0 +1,19 @@
+
+
+const fher = {
+    nombre: 'Fernando',
+    edad: 30,
+    imprimir() {
+        console.log()
+    }
+}
+
+const pedro = {
+    nombre: 'pedro',
+    edad: 15
+}
+
+const melissa = {
+    nombre: 'Melisa',
+    edad: 35
+}
