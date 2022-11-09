2e227bbee87b5f1a64849aad6e6762c1501b9c95
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2022-11-09 13:22:01 -0500

    UpdateSingletonEnJavaScript

--- JavaScript/01-fundamentos/assets/js/classes/singleton.js
+++ JavaScript/01-fundamentos/assets/js/classes/singleton.js
@@ -1,7 +1,37 @@
 
 
 class Singleton {
-    
+
+    static instancia; // es igual a undefined, pasa cuando se define la variable y no se le asigna valor
+    nombre = '';
+
+    constructor(nombre = '') {
+        
+        // // Esto es para aclara como funciona el booleano en una doble negación
+        // const a = undefined;
+        // console.log(a);
+        // console.log(!a);
+        // console.log(!!a);     
+
+        if (!!Singleton.instancia) {
+            return Singleton.instancia;
+        }
+
+        Singleton.instancia = this;
+        this.nombre = nombre;
+
+        // return this;
+
+    }
+
 }
+
+const instancia1 = new Singleton('Ironman');
+const instancia2 = new Singleton('Spiderman');
+const instancia3 = new Singleton('Black Panther');
+
+console.log(`Nombre en la instancia1 es: ${instancia1.nombre}`);
+console.log(`Nombre en la instancia2 es: ${instancia2.nombre}`);
+console.log(`Nombre en la instancia3 es: ${instancia3.nombre}`);
 
 
