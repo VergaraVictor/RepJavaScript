29ccf456fe124c2eab6ee0a1411f55ce623bc42e
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2023-02-03 11:52:30 -0500

    update04-blackjack-vite

---
+++ JavaScript/04-blackjack-vite/src/blackjack/usercases/crear-deck.js
@@ -0,0 +1,24 @@
+import _ from 'underscore';
+
+
+
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+    
+    let deck = [];
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tiposDeCarta) {
+            deck.push(i + tipo);
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of tiposEspeciales) {
+            deck.push(esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle(deck);
+
+    return deck;
+}
