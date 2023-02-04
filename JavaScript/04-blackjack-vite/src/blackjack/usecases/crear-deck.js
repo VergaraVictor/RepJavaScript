3d3d46f406d7af08eb9bce1073b0babd3b8b5a60
vvergara-hub <68487222+VergaraVictor@users.noreply.github.com>
2023-02-03 21:17:09 -0500

    jsdoc-refactorizar-codigo

--- JavaScript/04-blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ JavaScript/04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,9 +1,19 @@
 import _ from 'underscore';
 
-export const miNombre = 'Victor';
+// export const miNombre = 'Victor';
 
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo: ['C', 'D', 'H', 'S']
+ * @param {Array<String>} tiposEspeciales EJemplo: ['A', 'J', 'Q', 'K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
     
+    if (!tiposDeCarta || tiposDeCarta.length === 0) throw new Error('tiposDeCarta es obligatorio como un arreglo de string');
+    
+    if (!tiposEspeciales || tiposEspeciales.length === 0) throw new Error('tiposEspeciales es obligatorio como un arreglo de string');
+
     let deck = [];
 
     for (let i = 2; i <= 10; i++) {
