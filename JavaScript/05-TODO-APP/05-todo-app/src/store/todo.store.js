1febcddd8ca5eed77963886499d3df7c7ba56cc7
VergaraVictor <victorvergara1531@gmail.com>
2023-10-02 17:46:04 -0500

    MetodosQueOcuparemosDelStore

--- JavaScript/05-TODO-APP/05-todo-app/src/store/todo.store.js
+++ JavaScript/05-TODO-APP/05-todo-app/src/store/todo.store.js
@@ -22,7 +22,50 @@ const initStore = () => {
     console.log('initStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
 
 export default{
+    addTodo,
     initStore,
+    loadStore,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    setFilter,
+    toggleTodo,
 }
