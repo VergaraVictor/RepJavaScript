63d8c2a93fda5e9018a5fdc7039c3f9045a8de1f
VergaraVictor <victorvergara1531@gmail.com>
2023-10-09 18:30:38 -0500

    update toggleTodo

--- JavaScript/05-TODO-APP/05-todo-app/src/store/todo.store.js
+++ JavaScript/05-TODO-APP/05-todo-app/src/store/todo.store.js
@@ -13,6 +13,8 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,
 }
@@ -58,7 +60,13 @@ const addTodo = ( description ) => {
  * @param {String} todoId 
  */
 const toggleTodo = ( todoId ) => {
-    throw new Error('Not implemented');
+    
+    state.todos = state.todos.map( todo => {
+        if (todo.id === todoId ) {
+            todo.done = !todo.done;
+        }
+        return todo;
+    });
 }
 
 
