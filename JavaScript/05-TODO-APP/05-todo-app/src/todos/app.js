96f1a9cff726e23af933b3a3f49f6da96d50c9f4
VergaraVictor <victorvergara1531@gmail.com>
2023-10-09 21:09:08 -0500

    RenderizarlistadodeTodosv1

--- JavaScript/05-TODO-APP/05-todo-app/src/todos/app.js
+++ JavaScript/05-TODO-APP/05-todo-app/src/todos/app.js
@@ -1,17 +1,28 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const elementIDs = {
+    TodoList: '.todo-list',
+}
 
 /**
  * 
  * @param {String} elementId 
  */
 export const App = ( elementId ) => {
-    
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
+        renderTodos( elementIDs.TodoList, todos );
+    }
 
     //Cuando la función App() se llama
     (()=>{
         const app = document.createElement('div');
         app.innerHTML = html; // {{ name }}
         document.querySelector(elementId).append( app );
+        displayTodos();
     })();
 
 
---
+++ JavaScript/05-TODO-APP/05-todo-app/src/todos/use-cases/render-todos.js
@@ -0,0 +1,14 @@
+import { Todo } from "../models/todo.model";
+
+
+
+/**
+ * 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+
+    console.log(elementId, todos);
+
+}
