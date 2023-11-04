f42209efcdcfa0f5bcd617475f78605db7f7f088
VergaraVictor <victorvergara1531@gmail.com>
2023-11-04 09:25:08 -0500

    FetchAPI PeticiónHttpGet

--- JavaScript/07-http-app/src/breakingbad/breakingbad-app.js
+++ JavaScript/07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,12 +1,14 @@
 
 /**
- * @returns {Object} quote information
+ * @returns {Promise<Object>} quote information
  */
 const fetchQuote = async() => {
     
-    const res = await fetch('https://www.breakingbadapi.com/api/qoute/random');
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
 
-
+    console.log(data[0]);
+    return data[0];
 }
 
 
@@ -15,9 +17,11 @@ const fetchQuote = async() => {
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = ( element ) => {
+export const BreakingbadApp = async( element ) => {
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
 
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data!!';
 
 }
