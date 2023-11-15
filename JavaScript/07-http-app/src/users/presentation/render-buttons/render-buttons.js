fb1f8903b98974315ab46ce389fb92ef295ed229
VergaraVictor <victorvergara1531@gmail.com>
2023-11-14 19:06:15 -0500

    FuncionalidadDeLosBotones

--- JavaScript/07-http-app/src/users/presentation/render-buttons/render-buttons.js
+++ JavaScript/07-http-app/src/users/presentation/render-buttons/render-buttons.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
 import './render-buttons.css';
 
 /**
@@ -19,4 +20,18 @@ export const renderButtons = ( element ) => {
 
     element.append( prevButton, currentPageLabel, nextButton );
 
+    
+    nextButton.addEventListener('click', async() => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable( element );
+
+    });
+
+    prevButton.addEventListener('click', async() => {
+        await usersStore.loadPreviousPage();
+        currentPageLabel.innerText = usersStore.getCurrentPage();
+        renderTable( element );
+
+    });
 }
