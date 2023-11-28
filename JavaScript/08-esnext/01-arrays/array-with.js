ab72e2fe5185821fe7ab08fd704cf312d42dca37
VergaraVictor <victorvergara1531@gmail.com>
2023-11-28 10:49:49 -0500

    StructuredClone

--- JavaScript/08-esnext/01-arrays/array-with.js
+++ JavaScript/08-esnext/01-arrays/array-with.js
@@ -1,3 +1,28 @@
 
+const superHeroes = [
+    {
+        id: 1,
+        name:'Batman'
+    },
+    {
+        id: 2,
+        name:'Superman'
+    },
+    {
+        id: 3,
+        name:'Flash'
+    },
+    {
+        id: 4,
+        name:'Aquaman'
+    },
+];
+// antes era asi lo recomendable es el structuredClone
+// const array2 = JSON.parse( JSON.stringify(superHeroes))
 
-console.log('Hola Mundo');
+const superHeroesCopy = structuredClone(superHeroes);
+
+superHeroesCopy[0].name = 'Green Lantern'
+
+console.table( superHeroes );
+console.table(superHeroesCopy);
